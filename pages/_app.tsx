6c9d8db0f4600115a3de1b64ee98831fbfc0c86f
Admin <admin@Admins-MacBook-Pro.local>
2022-12-27 17:11:06 +0530

    add analitycs for view user

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/header/Header";
-import Footer from "../components/footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
