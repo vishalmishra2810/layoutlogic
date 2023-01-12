2a68a894db667dc7397e8b3ac640d6a4e89011ca
Admin <admin@Admins-MacBook-Pro.local>
2023-01-12 17:43:51 +0530

    question added javascript

--- database/allTech.ts
+++ database/allTech.ts
@@ -5,21 +5,6 @@ export enum ALL_TECH {
   HTML = "html",
   CSS = "css",
   SASS = "sass",
-  BOOTSTRAP = "bootstrap",
-  MATERIALUI = "materialui",
-  JQUERY = "jquery",
-  GIT = "git",
-  GITHUB = "github",
-  FIREBASE = "firebase",
-  NEXTJS = "nextjs",
   REDUX = "redux",
-  REDUXTOOLKIT = "reduxtoolkit",
   SCSS = "scss",
-}
-
-export enum ALL_COMPONENTS {
-  TABLE = "table",
-  CODE = "code",
-  IMAGE = "image",
-  LIST = "list",
 }
