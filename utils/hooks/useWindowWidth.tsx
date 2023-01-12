70040e874abfc0405c5c0a89aed1055497a1987b
Admin <admin@Admins-MacBook-Pro.local>
2023-01-12 16:29:15 +0530

    question added javascript

--- utils/hooks/useWindowWidth.tsx
+++ utils/hooks/useWindowWidth.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const useWindowWidth = () => {
-  const [width, setWidth]: any = useState();
+  const [width, setWidth]: any = useState(1400);
 
   useEffect(() => {
     setWidth(window.innerWidth);
