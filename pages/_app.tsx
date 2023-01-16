b16f7e318bcd3fadfa290dae8dff5526838f64f3
Admin <admin@Admins-MacBook-Pro.local>
2023-01-16 09:05:14 +0530

    reactjs questions added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,10 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/header/Header";
 import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6008576066337236"
-        crossOrigin="anonymous"
-      />
       <Header />
       <Component {...pageProps} />
       <Analytics />
