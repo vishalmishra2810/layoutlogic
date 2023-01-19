bc3dafe6516df972640d7e7a9c9602d489ca0188
Admin <admin@Admins-MacBook-Pro.local>
2023-01-19 21:28:24 +0530

    changes config file

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
   images: {
     domains: ["user-images.githubusercontent.com"],
   },
+  rewrites: [
+    { source: "/blog/", destination: "https://www.layoutlogic.com/blog/" },
+    {
+      source: "/blog/:match*",
+      destination: "https://www.layoutlogic.com/blog/:match*",
+    },
+    { source: "/", destination: "https://www.layoutlogic.com/" },
+    {
+      source: "/:match*",
+      destination: "https://www.layoutlogic.com/:match*",
+    },
+  ],
 };
 
 module.exports = nextConfig;
