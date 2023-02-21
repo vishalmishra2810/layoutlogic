0e6acd8ef486b88484047f83729c0161cf7afef1
Admin <admin@Admins-MacBook-Pro.local>
2023-02-21 09:46:21 +0530

    adding meta tags

--- pages/coding-question/index.tsx
+++ pages/coding-question/index.tsx
@@ -1,18 +1,27 @@
 import Head from "next/head";
 import React from "react";
 import TopCodingQuestion from "../../components/topCodingQuestions/TopCodingQuestion";
-import { META_TAGS } from "../../utils/constant";
 
 function TopFrontendQuestion() {
   return (
     <>
       <Head>
         <title>
-          Best 100+ Coding Question on Data Structure and Algorithm |
-          Layoutlogic
+          Best 100+ Coding Question on Data Structure and Algorithm for Frontend
+          Developer | Layoutlogic
         </title>
-        <meta name="description" content={META_TAGS.DESCRIPTION} />
-        <meta name="keywords" content={META_TAGS.KEYWORDS} />
+        <meta
+          name="description"
+          content="
+        Best 100+ Coding Question on Data Structure and Algorithm for Frontend Developer, Covered all the topics like Array, String, Linked List, Stack, Queue, Tree, Graph, Greedy, Dynamic Programming, Bit Manipulation, etc.
+        "
+        />
+        <meta
+          name="keywords"
+          content="
+        Best 100+ Coding Question on Data Structure and Algorithm for Frontend Developer, Array, String, Linked List, Stack, Queue, Tree, Graph, Greedy, Dynamic Programming, Bit Manipulation, etc.
+        "
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
